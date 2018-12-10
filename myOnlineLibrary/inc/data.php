3f5f3c2c899c1a636480e01ea15f2434564e83c9
alvin <alvinbhola007@icloud.com>
2018-12-10 18:26:59 -0500

    Add files via upload

---
+++ myOnlineLibrary/inc/data.php
@@ -0,0 +1,158 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+    "title" => "The Book of Joy",
+    "img" => "img/media/bookofjoy.jpg",
+    "genre" => "Self-help",
+    "format" => "Paperback",
+    "year" => 2016,
+    "category" => "Books",
+    "authors" => [
+      "Desmond Tutu",
+    "14th Dalai Lama",
+    "Douglas Abrams"
+    ],
+    "publisher" => "Penguin Publishing Group",
+    "isbn" => '9780399185045'
+];
+$catalog[102] = [
+    "title" => "Harry Potter",
+    "img" => "img/media/harrypotter123.jpg",
+    "genre" => "Fantasy",
+    "format" => "Digital",
+    "year" => 1998,
+    "category" => "Books",
+    "authors" => [
+        "J. K. Rowling"
+    ],
+    "publisher" => "Scholastic",
+    "isbn" => '	0-7475-3269-9 '
+];
+$catalog[103] = [
+    "title" => "Twilight",
+    "img" => "img/media/twilight.jpg",
+    "genre" => "Fantasy",
+    "format" => "Digital",
+    "year" => 2005,
+    "category" => "Books",
+    "authors" => [
+        "Stephenie Meyer"
+    ],
+    "publisher" => "Little, Brown and Company",
+    "isbn" => '0-316-16017-2'
+];
+$catalog[104] = [
+    "title" => "The Journey Home",
+    "img" => "img/media/theJourneyHome.jpg",
+    "genre" => "Non-Fiction",
+    "format" => "Digital",
+    "year" => 2008,
+    "category" => "Books",
+    "authors" => [
+        "Radhanath Swami"
+    ],
+    "publisher" => "2008 Mandala Publishing",
+    "isbn" => '978-1-60109-044-7'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Avatar",
+    "img" => "img/media/avatar.jpg",
+    "genre" => "Action",
+    "format" => "DVD/Streaming",
+    "year" => 2009,
+    "category" => "Movies",
+    "director" => "James Cameron",
+    "writers" => [
+        "James Cameron"
+    ],
+    "stars" => [
+        "Sam Worthington",
+        "Zoe Saldana",
+        "Sigourney Weaver",
+    ]
+];
+$catalog[202] = [
+    "title" => "American Sniper",
+    "img" => "img/media/americanSniper.jpg",
+    "genre" => "Action, Biography, Drama",
+    "format" => "DVD/Streaming",
+    "year" => 2014,
+    "category" => "Movies",
+    "director" => "Clint Eastwood",
+    "writers" => [
+      "Jason Hall",
+    "Chris Kyle"
+    ],
+    "stars" => [
+        "Bradley Cooper", "Sienna Miller", "Kyle Gallner"
+    ]
+];
+$catalog[203] = [
+    "title" => "The Lion King",
+    "img" => "img/media/theLionKing.jpeg",
+    "genre" => "Action",
+    "format" => "DVD",
+    "year" => 2015,
+    "category" => "Movies",
+    "director" => "Roger Allers", "Rob Minkoff",
+    "writers" => [
+        "Irene Mecchi",
+    ],
+    "stars" => [
+            "Matthew Broderick", "Jeremy Irons", "James Earl Jones"
+    ]
+];
+$catalog[204] = [
+    "title" => "Black Hawk Down",
+        "img" => "img/media/blackHawkDown.jpg",
+      "genre" => "Drama, Adventure",
+      "format" => "DVD/Streaming",
+      "year" => 2009,
+      "category" => "Movies",
+      "director" => "Ridley Scott",
+      "writers" => [
+          "Mark Bowden"
+      ],
+      "stars" => [
+          "Josh Hartnett", "Ewan McGregor", "Tom Sizemore"
+      ]
+    ];
+//Music
+$catalog[301] = [
+    "title" => "Beethoven: Complete Symphonies",
+    "img" => "img/media/bee.jpg",
+    "genre" => "Clasical",
+    "format" => "CD",
+    "year" => 2012,
+    "category" => "Music",
+    "artist" => "Ludwig van Beethoven"
+];
+$catalog[302] = [
+    "title" => "Born Sinner",
+    "img" => "img/media/bornSinner.jpg",
+    "genre" => "Hip-Hop",
+    "format" => "Streaming Services",
+    "year" => 2013,
+    "category" => "Music",
+    "artist" => "J. Cole"
+];
+$catalog[303] = [
+    "title" => "Kamekaze",
+    "img" => "img/media/kamekaze.jpg",
+    "genre" => "Hip Hop",
+    "format" => "Streaming Services",
+    "year" => 2018,
+    "category" => "Music",
+    "artist" => "Eminem"
+];
+$catalog[304] = [
+    "title" => "Dizzy up the girl",
+    "img" => "img/media/dizzyUpTheGirl.jpg",
+    "genre" => "Pop Rock",
+    "format" => "CD, Streaming Services",
+    "year" => 1998,
+    "category" => "Music",
+    "artist" => "Goo Goo Dolls"
+];
